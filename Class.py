98707826f39962481598bcbd44490f1088f96820
Kalashnikova Masha <127587584+Kot8k@users.noreply.github.com>
2023-03-25 09:40:20 +0300

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,13 @@
+name = input("Write your name:")
+class Cat:
+    def __input__(self, name: str, color: str, age: int, gender: str):
+        self.name = name
+        self.color = color
+        self.age = age
+        self.gander = gender
+
+    def wolk (self, other):
+        print(f" {self.name}: I want to go for a walk, {name}!")
+
+Chip = Cat("Chip", "orange", 1, "male")
+chip.walk (name)
